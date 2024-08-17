ed6a2a42b83b80a8a77c0b1640fa762754598e42
Nikolay <rozhnov.nv1992@gmail.com>
2024-08-17 12:42:32 +0700

    Добавьте файлы проекта.

---
+++ Task_struct&switch_3/Task_struct&switch_3.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <string>
+
+struct adress {
+	std::string city{};
+	std::string street{};
+	int house{};
+	int flat{};
+	int index{};
+};
+
+void print_adress(adress a) {
+	std::cout <<"Город: " << a.city << std::endl;
+	std::cout <<"Улица: " << a.street << std::endl;
+	std::cout <<"Номер дома: " << a.house << std::endl;
+	std::cout <<"Номер квартиры: " << a.flat << std::endl;
+	std::cout <<"Индекс: " << a.index << std::endl;
+	std::cout << std::endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	adress a{ "Новосибирск ","Горская", 1, 29, 163009 };
+	adress b{ "London", "Baker steet", 221, 1, 100001 };
+	print_adress( a);
+	print_adress( b);
+
+}
+
